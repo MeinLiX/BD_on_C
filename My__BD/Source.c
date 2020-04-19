061bcf02d8aee6934d49f93ed2673024ec3e46ea
RiXiLe <yuriks_k@ukr.net>
2020-04-19 21:05:46 +0300

    insert, print Slave

--- My__BD/Source.c
+++ My__BD/Source.c
@@ -4,24 +4,26 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define DontDeleted 0
 #define TempDeleted 1
 #define TotalDeleted 2
-#define DontDeleted 0
+
 #define LEN 40
-#define MAXplayersINorg 5
-#define Organisation_data_file_name "Organisation_data.txt" //master
-#define Organisation_shift_file_name "Organisation_shift.txt" //master
-#define Player_data_file_name "Player_data.txt" //slave
+#define MAXSlavesINorg 5
+
+#define Master_data_file_name "Master_data.txt" //master
+#define Master_shift_file_name "Master_shift.txt" //master shift
+#define Slave_data_file_name "Slave_data.txt" //slave
 
 //slave
-typedef struct Player {
+typedef struct Slave_struct {
 	int id;
 	char firstname[LEN];
 	char lastname[LEN];
 	char nickname[LEN];
 	short deleted;
-	int compID;
-}player;
+	int MasterID;
+}Slave;
 
 //master
 struct My_date {
@@ -29,49 +31,71 @@ struct My_date {
 	int month;
 	int year;
 };
-typedef struct Ñompany {
+typedef struct Master_struct {
 	int key;
 	char name[LEN];
 	char country[LEN];
 	struct My_date date;
 	short deleted;
-	int players[MAXplayersINorg];
-}comp;
-typedef struct Ñompany_shift {
+	int Slaves[MAXSlavesINorg];
+}Master;
+typedef struct Master_shift_struct {
 	int key;
 	int address;
 	short deleted;
-}comp_shift;
+}Master_shift;
 
 //slave
-/*void Insert_S() {//Organisation
-	printf("Creating Player;\nEnter key, firstname, lastname, nickname:\n");
-	FILE* f = fopen(Player_data_file_name, "rb+");
-	player temp;
-	scanf("%d%40s%40s%40s", &(temp.id), &(temp.firstname), &(temp.lastname), &(temp.nickname));
-	temp.deleted = DontDeleted;
-	fseek(f, 0, SEEK_END);
-	fwrite(&temp, sizeof(player), 1, f);
-
-	printf("\n");
-	printf("Success Insert!\n\n");
+void print_S(Slave* print) {
+	if (print->deleted == DontDeleted)
+		printf("\n\nid: %d, %s :%s (%s)\nMaster Key: %d\n", print->id, print->firstname, print->lastname, print->nickname , print->MasterID);
+	else if (print->deleted == TempDeleted)
+		printf("\n\nid: %d, %s :%s (%s)\n", print->id, print->firstname, print->lastname, print->nickname);
+	else
+		printf("\n\Slave not found!\n");
+	return;
+}
+void print_S_All()
+{
+	FILE* f = fopen(Slave_data_file_name, "rb");
+	Slave print;
+	int Lastid = -1;
+	while (!(feof(f)))
+	{
+		fread(&print, sizeof(Slave), 1, f);
+		if (Lastid != print.id)
+		{
+			print_S(&print);
+		}
+		Lastid = print.id;
+	}
 	fclose(f);
 	return;
 }
-*/
-
+void Insert_S() {
+	printf("Creating Slave;\nEnter id, firstname, lastname, nickname:\n");
+	FILE* f = fopen(Slave_data_file_name, "rb+");
+	Slave temp;
+	scanf("%d%s%s%s", &(temp.id), &(temp.firstname), &(temp.lastname), &(temp.nickname));
+	temp.deleted = TempDeleted; // no active
+	fseek(f, 0, SEEK_END);
+	fwrite(&temp, sizeof(Slave), 1, f);
+	printf("\nSuccess Insert!\n\n");
+	fclose(f);
+	return;
+}
 
 //master
-void shift_update(comp* temp)
+void shift_update(Master* temp)
 {
-	FILE* f_s = fopen(Organisation_shift_file_name, "rb+");
-	comp_shift arr[100];
+	FILE* f_s = fopen(Master_shift_file_name, "rb+");
+	Master_shift arr[100];
 	fseek(f_s, 0, SEEK_SET);
 	int i;
 	short shifts = 0;
 	for (i = 1; !feof(f_s); i++)
 	{
-		fread(&arr[i], sizeof(comp_shift), 1, f_s);
+		fread(&arr[i], sizeof(Master_shift), 1, f_s);
 		if (arr[i].key == temp->key)
 		{
 			arr[i].deleted = TotalDeleted;
@@ -84,30 +108,30 @@ void shift_update(comp* temp)
 	fclose(f_s);
 	if (!shifts)
 		return;
-	FILE* fw = fopen(Organisation_shift_file_name, "wb");
+	FILE* fw = fopen(Master_shift_file_name, "wb");
 	fseek(fw, 0, SEEK_SET);
 	for (int j = 1; arr[j].key != -1; j++)
 	{
 		if (arr[j].deleted == TotalDeleted)
 			continue;
-		fwrite(&arr[j], sizeof(comp_shift), 1, fw);
+		fwrite(&arr[j], sizeof(Master_shift), 1, fw);
 	}
 
 	fclose(fw);
 }
-void Delete_M_Merm()
+void Delete_M_Perm()
 {
-	printf("Deleting Organisation;\nEnter organisation key:");
+	printf("\nDeleting Master;\nEnter Master key:");
 	int key_;
 	scanf("%d", &key_);
-	FILE* f = fopen(Organisation_data_file_name, "rb+");
-	comp arr[100];
+	FILE* f = fopen(Master_data_file_name, "rb+");
+	Master arr[100];
 	fseek(f, 0, SEEK_SET);
 	int i;
 	double founded = false;
 	for (i = 1; !feof(f); i++)
 	{
-		fread(&arr[i], sizeof(comp), 1, f);
+		fread(&arr[i], sizeof(Master), 1, f);
 		if (arr[i].key == key_)
 		{
 			arr[i].deleted = TotalDeleted;
@@ -121,7 +145,7 @@ void Delete_M_Merm()
 		printf("\nThe key was not found!\n");
 		return;
 	}
-	FILE* fw = fopen(Organisation_data_file_name, "wb");
+	FILE* fw = fopen(Master_data_file_name, "wb");
 	fseek(fw, 0, SEEK_SET);
 	for (int j = 1; arr[j].key != -1; j++)
 	{
@@ -130,7 +154,7 @@ void Delete_M_Merm()
 			shift_update(&arr[j]);
 			continue;
 		}
-		fwrite(&arr[j], sizeof(comp), 1, fw);
+		fwrite(&arr[j], sizeof(Master), 1, fw);
 	}
 	fclose(fw);
 	return;
@@ -138,17 +162,17 @@ void Delete_M_Merm()
 
 void Delete_M_temp()
 {
-	printf("Temp deleting Organisation;\nEnter organisation key:");
+	printf("\nTemp deleting Master;\nEnter Master key:");
 	int key_;
 	scanf("%d", &key_);
-	FILE* f = fopen(Organisation_data_file_name, "rb+");
-	comp arr[100];
+	FILE* f = fopen(Master_data_file_name, "rb+");
+	Master arr[100];
 	fseek(f, 0, SEEK_SET);
 	int i;
 	bool founded = false;
 	for (i = 1; !feof(f); i++)
 	{
-		fread(&arr[i], sizeof(comp), 1, f);
+		fread(&arr[i], sizeof(Master), 1, f);
 		if (arr[i].key == key_)
 		{
 			founded = true;
@@ -161,28 +185,28 @@ void Delete_M_temp()
 		return;
 	}
 	arr[i - 1].key = -1;
-	FILE* fw = fopen(Organisation_data_file_name, "wb");
+	FILE* fw = fopen(Master_data_file_name, "wb");
 	fseek(fw, 0, SEEK_SET);
 	for (int j = 1; arr[j].key != -1; j++)
 	{
-		fwrite(&arr[j], sizeof(comp), 1, fw);
+		fwrite(&arr[j], sizeof(Master), 1, fw);
 	}
 	fclose(fw);
 	return;
 }
-void Remove_Delete_M_temp() 
+void Recovery_M() 
 {
-	printf("Recover Organisation;\nEnter organisation key:");
+	printf("\nRecover Master;\nEnter Master key:");
 	int key_;
 	scanf("%d", &key_);
-	FILE* f = fopen(Organisation_data_file_name, "rb+");
-	comp arr[100];
+	FILE* f = fopen(Master_data_file_name, "rb+");
+	Master arr[100];
 	fseek(f, 0, SEEK_SET);
 	int i;
 	bool founded = false;
 	for (i = 1; !feof(f); i++)
 	{
-		fread(&arr[i], sizeof(comp), 1, f);
+		fread(&arr[i], sizeof(Master), 1, f);
 		if (arr[i].key == key_)
 		{
 			founded = true;
@@ -196,64 +220,64 @@ void Remove_Delete_M_temp()
 		return;
 	}
 	arr[i - 1].key = -1;
-	FILE* fw = fopen(Organisation_data_file_name, "wb");
+	FILE* fw = fopen(Master_data_file_name, "wb");
 	fseek(fw, 0, SEEK_SET);
 	for (int j = 1; arr[j].key != -1; j++)
 	{
-		fwrite(&arr[j], sizeof(comp), 1, fw);
+		fwrite(&arr[j], sizeof(Master), 1, fw);
 	}
 	fclose(fw);
 	return;
 }
 
-void printComp(comp* print)
+void print_M(Master* print)
 {
 	if (print->deleted == DontDeleted)
 		printf("\n\nKey:%d, Name:%s, Country:%s, Foundation date: %d.%d.%d\nStatus: Active!\n", print->key, print->name, print->country, print->date.day, print->date.month, print->date.year);
 	else if (print->deleted == TempDeleted)
 		printf("\nKey:%d, Name:%s\nStatus: temporary Deleted!\n", print->key, print->name);
 	else
-		printf("\n\nStruct not found!\n");
+		printf("\n\nMaster not found!\n");
 	return;
 }
-void printCompS(comp_shift* print) 
+void print_M_Sift(Master_shift* print) 
 {
 	if (print->deleted == DontDeleted)
 		printf("\n\nKey:%d, Adress:%d\nStatus: Active!\n", print->key, print->address);
 	else if (print->deleted == TempDeleted)
 		printf("\n\nKey:%d, Adress:%d\nStatus: temporary Deleted!\n", print->key, print->address);
 	else
-		printf("\n\nStruct not found!\n");
+		printf("\n\nMaster_shift not found!\n");
 	return;
 }
-void printCompAll()
+void print_M_All()
 {
-	FILE* f = fopen(Organisation_data_file_name, "rb");
-	comp print;
+	FILE* f = fopen(Master_data_file_name, "rb");
+	Master print;
 	int LastKey = -1;
 	while (!(feof(f)))
 	{
-		fread(&print, sizeof(comp), 1, f);
+		fread(&print, sizeof(Master), 1, f);
 		if (LastKey != print.key)
 		{
-			printComp(&print);
+			print_M(&print);
 		}
 		LastKey = print.key;
 	}
 	fclose(f);
 	return;
 }
-void printCompAllS()
+void print_M_AllShift()
 {
-	FILE* f = fopen(Organisation_shift_file_name, "rb");
-	comp_shift print;
+	FILE* f = fopen(Master_shift_file_name, "rb");
+	Master_shift print;
 	int LastKey = -1;
 	while (!(feof(f)))
 	{
-		fread(&print, sizeof(comp_shift), 1, f);
+		fread(&print, sizeof(Master_shift), 1, f);
 		if (LastKey != print.key)
 		{
-			printCompS(&print);
+			print_M_Sift(&print);
 		}
 		LastKey = print.key;
 	}
@@ -263,43 +287,43 @@ void printCompAllS()
 
 void Insert_M()
 {
-	printf("Creating Organisation;\nEnter key, name, country, fundation data(dd mm yyyy):\n");
-	FILE* f = fopen(Organisation_data_file_name, "rb+");
-	comp temp;
-	scanf("%d%40s%40s%d%d%d", &(temp.key), &(temp.name), &(temp.country), &(temp.date.day), &(temp.date.month), &(temp.date.year));
+	printf("\nCreating Master;\nEnter key, name, country, fundation data(dd mm yyyy):\n");
+	FILE* f = fopen(Master_data_file_name, "rb+");
+	Master temp;
+	scanf("%d%s%s%d%d%d", &(temp.key), &(temp.name), &(temp.country), &(temp.date.day), &(temp.date.month), &(temp.date.year));
 	temp.deleted = DontDeleted;
-	for (int q = 0; q < MAXplayersINorg; q++)
+	for (int q = 0; q < MAXSlavesINorg; q++)
 	{
-		temp.players[q] = 0;
+		temp.Slaves[q] = 0;
 	}
 	fseek(f, 0, SEEK_END);
-	fwrite(&temp, sizeof(comp), 1, f);
+	fwrite(&temp, sizeof(Master), 1, f);
 
-	FILE* f_s = fopen(Organisation_shift_file_name, "rb+");
-	comp_shift temp_s;
+	FILE* f_s = fopen(Master_shift_file_name, "rb+");
+	Master_shift temp_s;
 	temp_s.key = temp.key;
-	temp_s.address = ftell(f) / sizeof(comp);
+	temp_s.address = ftell(f) / sizeof(Master);
 	temp_s.deleted = DontDeleted;
 	fseek(f_s, 0, SEEK_END);
-	fwrite(&temp_s, sizeof(comp_shift), 1, f_s);
+	fwrite(&temp_s, sizeof(Master_shift), 1, f_s);
 	printf("\n");
-	printComp(&temp);
-	printf("Success Insert!\n\n");
+	print_M(&temp);
+	printf("\nSuccess Insert!\n\n");
 	fclose(f);
 	fclose(f_s);
 	return;
 }
 void Update_M()
 {
-	printf("Updating Organisation;\nEnter organisation key(for update data):\n");
-	FILE* f_s = fopen(Organisation_shift_file_name, "rb");
+	printf("\nUpdating Master;\nEnter Master key:\n");
+	FILE* f_s = fopen(Master_shift_file_name, "rb");
 	int key_;
 	scanf("%d", &key_);
-	comp_shift temp_s;
+	Master_shift temp_s;
 	bool flag = 0;
 	while (!feof(f_s))
 	{
-		fread(&temp_s, sizeof(comp_shift), 1, f_s);
+		fread(&temp_s, sizeof(Master_shift), 1, f_s);
 		if (temp_s.key == key_)
 		{
 			flag = 1;
@@ -308,23 +332,23 @@ void Update_M()
 	}
 	if (!flag)
 	{
-		printf("Struct don't founded!\n");
+		printf("Master don't found!\n");
 		return;
 	}
-	FILE* f = fopen(Organisation_data_file_name, "rb");
-	comp temp;
-	comp scan;
-	fseek(f, sizeof(comp) * (temp_s.address - 1), 1, SEEK_SET);
-	fread(&temp, sizeof(comp), 1, f);
+	FILE* f = fopen(Master_data_file_name, "rb");
+	Master temp;
+	Master scan;
+	fseek(f, sizeof(Master) * (temp_s.address - 1), 1, SEEK_SET);
+	fread(&temp, sizeof(Master), 1, f);
 	if (temp.deleted == TempDeleted)
 	{
-		printComp(&temp);
+		print_M(&temp);
 		return;
 	}
 	printf("\nEnter name(0 if don't change):");
-	scanf("%40s", &(scan.name));
+	scanf("%s", &(scan.name));
 	printf("\nEnter country(0 if don't change):");
-	scanf("%40s", &(scan.country));
+	scanf("%s", &(scan.country));
 	printf("\nEnter date(dd mm yyyy)(0 0 0 if don't change):");
 	scanf("%d%d%d", &(scan.date.day), &(scan.date.month), &(scan.date.year));
 	if (strcmp(scan.name, "0"))
@@ -343,28 +367,28 @@ void Update_M()
 	}
 	fclose(f);
 	//Delete_T_Mointer_f(temp_s.address); //don't need
-	FILE* fw = fopen(Organisation_data_file_name, "rb+");
-	fseek(fw, sizeof(comp) * (temp_s.address - 1), 1, SEEK_SET);
-	fwrite(&temp, sizeof(comp), 1, fw);
-	printComp(&temp);
+	FILE* fw = fopen(Master_data_file_name, "rb+");
+	fseek(fw, sizeof(Master) * (temp_s.address - 1), 1, SEEK_SET);
+	fwrite(&temp, sizeof(Master), 1, fw);
+	print_M(&temp);
 	printf("Success Update!\n");
 	fclose(fw);
 	return;
 }
 
-comp Get_M_r(int key_) {
-	FILE* f_s = fopen(Organisation_shift_file_name, "rb");
-	comp_shift temp_s;
+Master Get_M(int key_) {
+	FILE* f_s = fopen(Master_shift_file_name, "rb");
+	Master_shift temp_s;
 	bool flag = 0;
 	while (!feof(f_s)) {
-		fread(&temp_s, sizeof(comp_shift), 1, f_s);
+		fread(&temp_s, sizeof(Master_shift), 1, f_s);
 		if (temp_s.key == key_) {
 			flag = 1;
 			break;
 		}
 	}
 	fclose(f_s);
-	comp temp_d;
+	Master temp_d;
 	temp_d.name[0] = '-1';
 	temp_d.country[0] = '-1';
 	temp_d.date.day = -1;
@@ -374,31 +398,31 @@ comp Get_M_r(int key_) {
 	if (!flag) {
 		return temp_d;
 	}
-	FILE* f = fopen(Organisation_data_file_name, "rb");
-	comp temp;
-	fseek(f, sizeof(comp) * (temp_s.address - 1), 1, SEEK_SET);
-	fread(&temp, sizeof(comp), 1, f);
+	FILE* f = fopen(Master_data_file_name, "rb");
+	Master temp;
+	fseek(f, sizeof(Master) * (temp_s.address - 1), 1, SEEK_SET);
+	fread(&temp, sizeof(Master), 1, f);
 	fclose(f);
 	return temp;
 }
-void Get_M() {
-	printf("Enter organisation key(for get data):\n");
-	FILE* f_s = fopen(Organisation_shift_file_name, "rb");
+void Print_M() {
+	printf("\nGet(print) Master;\nEnter Master key:");
+	FILE* f_s = fopen(Master_shift_file_name, "rb");
 	int key_;
 	scanf("%d", &key_);
-	comp_shift temp_s;
+	Master_shift temp_s;
 	bool flag = 0;
 	//int ch = getc(f_s);
 	// (ch != EOF)
 	while (!feof(f_s)) {
-		fread(&temp_s, sizeof(comp_shift), 1, f_s);
+		fread(&temp_s, sizeof(Master_shift), 1, f_s);
 		if (temp_s.key == key_) {
 			flag = 1;
 			break;
 		}
 	}
 	fclose(f_s);
-	comp temp_d;
+	Master temp_d;
 	temp_d.name[0] = '-1';
 	temp_d.country[0] = '-1';
 	temp_d.date.day = -1;
@@ -406,48 +430,47 @@ void Get_M() {
 	temp_d.date.year = -1;
 	temp_d.deleted = TotalDeleted;
 	if (!flag) {
-		printComp(&temp_d);
+		print_M(&temp_d);
 		return;
 	}
-	FILE* f = fopen(Organisation_data_file_name, "rb");
-	comp temp;
-	fseek(f, sizeof(comp) * (temp_s.address - 1), 1, SEEK_SET);
-	fread(&temp, sizeof(comp), 1, f);
+	FILE* f = fopen(Master_data_file_name, "rb");
+	Master temp;
+	fseek(f, sizeof(Master) * (temp_s.address - 1), 1, SEEK_SET);
+	fread(&temp, sizeof(Master), 1, f);
 	fclose(f);
-	printComp(&temp);
+	print_M(&temp);
 	return;
 }
 
 int main() {
 	while (1) {
-		printCompAll();
-		//printCompAllS();
+		Insert_S();
+		Insert_S();
+		Insert_S();
+		print_S_All();
 		system("pause");
 	}
 	return 0;
 }
 //---------------------------------------------Functional
 
-	//printCompAll(); //work +
-	//printCompAllS(); //work +
+	//print_M_All(); //work +
+	//print_M_AllShift(); //work +
+
 	//Insert_M();  //work + 
 	//Update_M(); //work +
-	//Get_M(); //work +
-	//Get_M_r(); //work +
 
-	//comp My = Get_M();	//work +
-	//printComp(&My);  //work +
+	//Print_M(); //work +
+	//Master My = Get_M();	//work +
+	//print_M(&My);  //work +
 
 	//Delete_M_temp(); //work +
-	//Remove_Delete_M_temp(); //work +
+	//Recovery_M(); //work +
 
-	//Delete_M_Merm(); //work +
-
-	//temp_total_delete(); //cleaner need realise
-
+	//Delete_M_Perm(); //work +
 /*
 organisation + client.
-Master(organisation file){update insert get} and Slave (player file){update insert get}
+Master(organisation file){update insert get} and Slave (Slave file){update insert get}
 
-Slave have unique key and i can write all player key in Organisation array!!!
+Slave have unique key and i can write all Slave key in Organisation array!!!
 */
